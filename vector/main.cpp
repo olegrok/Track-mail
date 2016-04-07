7cec6500d7c627153de522884614391516e84031
olegrok <babinoleg@mail.ru>
2016-04-07 13:50:41 +0300

    add algebraic operations

--- vector/main.cpp
+++ vector/main.cpp
@@ -5,7 +5,25 @@ int main()
 {
   Vector<int> a(0);
   Vector<int> b = a;
-  for(int i = 0; i < 10; i++){
+  Vector<int> ex1(12);
+  Vector<int> ex2(12);
+  for(int i = 0; i < 12; i++){
+    ex1[i] = rand() % 10;
+    std::cout << "ex1[" << i << "]= " << ex1[i] << std::endl;
+    ex2[i] = rand() % 10;
+    std::cout << "ex2[" << i << "]= " << ex2[i] << std::endl;
+  }
+  Vector<int> ex3(12);
+  ex3 = ex1 + ex2;
+  for(int i = 0; i < 12; i++)
+    std::cout << "ex3[" << i << "]= " << ex3[i] << std::endl;
+  ex3 -= ex3;
+  for(int i = 0; i < 12; i++)
+    std::cout << "ex3[" << i << "]= " << ex3[i] << std::endl;
+  Vector<int> v4 = ex3 + 2 * ex1;
+  for(int i = 0; i < 12; i++)
+    std::cout << "ex4[" << i << "]= " << v4[i] << std::endl;
+  /*for(int i = 0; i < 10; i++){
     b.push_back(rand() % 10);
     std::cout << b.back() << std::endl;
   }
@@ -19,6 +37,6 @@ int main()
   Vector<int> d(Vector<int>(15));
   std::cout << a.size() << std::endl;
   a.push_back(15);
-  a.push_back(2);
+  a.push_back(2);*/
   return 0;
 }
--- vector/vector.hpp
+++ vector/vector.hpp
@@ -3,6 +3,7 @@
 
 #include  <algorithm>
 #include <stdexcept>
+#include <iostream>
 
 template <class Type>
 class Vector{
@@ -10,18 +11,53 @@ public:
   Vector(int size = 0);
   Vector(const Vector<Type> &copy);
   Vector(Vector &&move);
-  Vector<Type>& operator=(const Vector<Type>& copy);
   Vector<Type>& operator=(Vector<Type> copied);
   ~Vector();
-  int size();
-  int max_size();
-  bool resize(int newSize);
-  bool empty();
-  Type& operator[](int);
-  Type& at(int );
-  void push_back(Type data);
+  int size() const;
+  int max_size() const;
+  bool resize(const int newSize);
+  bool empty() const;
+  Type& operator[](const int);
+  Type& at(const int);
+  void push_back(const Type data);
   void pop_back();
-  Type back();
+  Type& back();
+  Vector<Type>& operator+=(const Vector<Type> v);
+  Vector<Type>& operator-=(const Vector<Type> v);
+
+  friend Vector<Type>& operator*(Vector<Type> v, const Type num){
+    for(int i = 0; i < v._size; i++)
+      v._array[i] *= num;
+    return v;
+  }
+
+  friend Vector<Type>& operator*(const Type num, Vector<Type>& v){
+    for(int i = 0; i < v._size; i++)
+      v._array[i] *= num;
+    return v;
+  }
+
+  friend Type operator*(const Vector<Type> &left, const Vector<Type>& right){
+    if(left._size != right._size)
+      throw std::length_error("Different length of vectors");
+    Type result;
+    Vector<Type> sum(left._size);
+    for(int i = 0; i < sum._size; i++){
+      result += right._array[i] * left._array[i];
+    }
+    return result;
+  }
+
+  friend const Vector<Type> operator+(const Vector<Type> &left, const Vector<Type>& right){
+    if(left._size != right._size)
+      throw std::length_error("Different length of vectors");
+    Vector<Type> sum(left._size);
+    for(int i = 0; i < sum._size; i++){
+      sum._array[i] = right._array[i] + left._array[i];
+    }
+    return sum;
+  }
+
 
 private:
   Type* _array;
@@ -32,7 +68,7 @@ private:
 
 template <class Type>
 Vector<Type>::Vector(int size) :
-  _size(0),
+  _size(size),
   _max_size(size >= 0 ? size : 1)
 {
   _array = (_max_size ? new Type[_max_size] : 0);
@@ -53,17 +89,9 @@ Vector<Type>::Vector(Vector &&move){
 }
 
 template <class Type>
-Vector<Type>& Vector<Type>::operator=(const Vector<Type>& copy)
-{
-    Vector copied(copy);
-    swap(*this, copied);
-    return *this;
-}
-
-template <class Type>
 Vector<Type>& Vector<Type>::operator=(Vector<Type> copied)
 {
-    swap(*this, copied);
+    _swap(*this, copied);
     return *this;
 }
 
@@ -80,12 +108,12 @@ void Vector<Type>::_swap(Vector& first, Vector& second){
 }
 
 template<class Type>
-int Vector<Type>::size(){
+int Vector<Type>::size() const{
   return _size;
 }
 
 template<class Type>
-int Vector<Type>::max_size(){
+int Vector<Type>::max_size() const{
   return _max_size;
 }
 
@@ -104,7 +132,7 @@ bool Vector<Type>::resize(int newSize){
 }
 
 template<class Type>
-bool Vector<Type>::empty(){
+bool Vector<Type>::empty() const{
   return _size;
 }
 
@@ -138,10 +166,45 @@ void Vector<Type>::pop_back(){
 }
 
 template <class Type>
-Type Vector<Type>::back(){
+Type& Vector<Type>::back(){
   if(!_size)
     throw std::out_of_range("Out of range");
   return _array[_size - 1];
 }
 
+template <class Type>
+Vector<Type>& Vector<Type>::operator+=(const Vector<Type> v){
+  if(_size != v._size)
+    throw std::length_error("Different length of vectors");
+  for(int i = 0; i < _size; i++)
+    _array[i] += v[i]._array;
+  return *this;
+}
+
+template <class Type>
+Vector<Type>& Vector<Type>::operator-=(const Vector<Type> v){
+  if(_size != v._size)
+    throw std::length_error("Different length of vectors");
+  for(int i = 0; i < _size; i++)
+    _array[i] -= v._array[i];
+  return *this;
+}
+
+/*template <class Type>
+Vector<Type>& Vector<Type>::operator*(const Type num){
+  for(int i = 0; i < _size; i++)
+    _array[i] *= num;
+  return *this;
+}*/
+/*template <class Type>
+const Vector<Type> Vector<Type>::operator+(const Vector<Type> &left, const Vector<Type>& right){
+  if(left._size != right._size)
+    throw std::length_error("Different length of vectors");
+  Vector<Type> sum(left._size);
+  for(int i = 0; i < sum._size; i++){
+    sum._array[i] = right._array[i] + left._array[i];
+    std::cout << "v1 + v2 = " << sum._array[i] << std::endl;
+  }
+  return sum;
+}*/
 #endif
