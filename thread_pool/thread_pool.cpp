59ec99b6373d5576cd4c048fd777ae57bf258e0e
olegrok <babinoleg@mail.ru>
2016-04-15 07:48:41 +0300

    error class was added

--- thread_pool/thread_pool.cpp
+++ thread_pool/thread_pool.cpp
@@ -6,6 +6,56 @@
 #include <iostream>
 #include <cstdlib>
 #include <atomic>
+#include <system_error>
+#include <string>
+
+enum class DataBaseErr : int {
+  ok = 7000,
+  can_not_create = 7001,
+  can_not_open = 7002,
+  wrong_request = 7003,
+  can_not_insert = 7004,
+  can_not_delete = 7005
+};
+
+namespace std{
+  template<> struct is_error_code_enum<DataBaseErr> : public true_type{};
+}
+
+class DataBase_category_impl : public std::error_category {
+public:
+  virtual const char* name() const noexcept { return "DataBase"; };
+  virtual std::string message(int ev) const;
+};
+
+std::string DataBase_category_impl::message(int ev) const
+{
+  switch (ev) {
+    case (int)DataBaseErr::ok : return "OK";
+    case (int)DataBaseErr::can_not_create: return "Unable to create a table";
+    case (int)DataBaseErr::can_not_open: return "Unable to open data base";
+    case (int)DataBaseErr::wrong_request: return "Wrong request";
+    case (int)DataBaseErr::can_not_insert: return "Unable to make insert opeation";
+    case (int)DataBaseErr::can_not_delete: return "Unable to make delete opeation";
+    default: return "Unknown Data Base error";
+  }
+}
+
+const std::error_category& DataBase_category(){
+  static DataBase_category_impl instance;
+  return instance;
+}
+
+
+
+std::error_code make_error_code(DataBaseErr e){
+  return std::error_code(static_cast<int>(e), DataBase_category());
+}
+
+std::error_condition make_error_condition(DataBaseErr e){
+  return std::error_condition(static_cast<int>(e), DataBase_category());
+}
+
 
 typedef void (*Task)(void*);
 
@@ -42,27 +92,23 @@ private:
 
 thread_pool::thread_pool(int thread_num) :
   _thread_num(thread_num),
-  _active(0),
+  _active(thread_num),
   _end(false)
 {
   if(thread_num > 0)
     for(int i = 0; i < thread_num; i++)
       slaves.push_back(std::thread(_slave, std::ref(tasksLock), std::ref(cv), std::ref(_tasks), std::ref(_active), std::ref(_end)));
+    while(_active != 0);
 }
 
 thread_pool::~thread_pool(){
-  while(_active != 0){
-    tasksLock.lock();
-    if(_tasks.empty() == true){
-      tasksLock.unlock();
-      break;
-    }
-    tasksLock.unlock();
-  }
+  while(_active != 0);
   _end = true;
   cv.notify_all();
   for(auto& thread : slaves){
-    thread.join();}
+    std::cout << "join" << std::endl;
+    thread.join();
+  }
 }
 
 void thread_pool::post(Task task, void* taskData){
@@ -76,17 +122,27 @@ void thread_pool::_slave(std::mutex& mutex, std::condition_variable& cond, std::
   std::atomic<int>& _active, std::atomic<bool>& _end){
   Task task;
   void* arg;
+
+  _active--;
+  while(_active != 0);
+  std::cout << _active << std::endl;
+
   mutex.lock();
   std::cout << "thread " << std::this_thread::get_id() << " created" << std::endl;
   mutex.unlock();
 
   std::unique_lock<std::mutex> locker(mutex);
-  while(_end != true) {
+
+  while(_end == false){
      cond.wait(locker, [&](){
-       return !v.empty();
+       bool empty = !v.empty();
+       return (empty || _end);
      });
-     if(_end == true)
-      break;
+     if(_end == true){
+//        std::cout << "notified" << std::endl;
+        break;
+      }
+
      _active++;
     task = v.back()._task;
     arg = v.back()._arg;
@@ -95,16 +151,22 @@ void thread_pool::_slave(std::mutex& mutex, std::condition_variable& cond, std::
     task(arg);
     _active--;
   }
+  mutex.unlock();
+  std::cout << "joined " << std::this_thread::get_id() << std::endl;
 }
 
 int main(){
+  /*std::error_code e;
+  e = DataBaseErr::can_not_open;
+  std::cout << e.message() << std::endl;*/
   int a = 0;
   thread_pool pool(5);
   pool.post(test, NULL);
   pool.post(test, NULL);
+  //std::cin >> a;
+  pool.post(test, NULL);
+  pool.post(test, NULL);
+  pool.post(test, NULL);
   std::cin >> a;
-  pool.post(test, NULL);
-  pool.post(test, NULL);
-  pool.post(test, NULL);
   return 0;
 }
